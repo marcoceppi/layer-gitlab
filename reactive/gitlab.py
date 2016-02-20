08c3060529b5f227086f3e08232047da7a2e0427
Marco Ceppi <marco@ceppi.net>
2016-02-20 15:29:32 -0500

    python cleanups

--- reactive/gitlab.py
+++ reactive/gitlab.py
@@ -1,13 +1,15 @@
-import fileinput
-import sys
-from charmhelpers.fetch import apt_install
-from charms.reactive import when, when_not, set_state, remove_state, hook
-from subprocess import check_call, CalledProcessError, call
-from charmhelpers.core import hookenv
-from charmhelpers.core.hookenv import status_set
-from charms.reactive.helpers import data_changed
 import re
+import sys
 import shutil
+import fileinput
+
+from charmhelpers.fetch import apt_install, apt_update
+from charmhelpers.core.hookenv import status_set, config
+
+from charms.reactive import when, when_not, set_state, remove_state, hook
+from charms.reactive.helpers import data_changed
+
+from subprocess import check_call, CalledProcessError, call
 
 
 @when_not('gitlab.installed')
@@ -18,14 +20,14 @@ def install():
     check_call(['apt-key', 'add', './data/gitlab_gpg.key'])
 
     shutil.copy2('data/gitlab_gitlab-ce.list', '/etc/apt/sources.list.d/gitlab-ce.list')
-
-    check_call(['apt-get', 'update'])
+    apt_update()
 
     version = ''
-    if hookenv.config('gitlab_version'):
-        version = '=' + hookenv.config('gitlab_version')
+    if config('gitlab_version'):
+        version = '=' + config('gitlab_version')
 
-    check_call(['apt-get', 'install', 'gitlab-ce' + version])
+    apt_install(['gitlab-ce' + version])
+
     check_call(['gitlab-ctl', 'reconfigure'])
     set_state('gitlab.installed')
     status_set('active', 'GitLab is ready!')
@@ -33,14 +35,14 @@ def install():
 
 @when('gitlab.installed')
 def check_running():
-    if data_changed('gitlab.config', hookenv.config()):
+    if data_changed('gitlabconfig', config()):
         status_set('maintenance', 'Updating Config')
-        updateConfig(hookenv.config())
+        updateConfig(config())
 
-    if hookenv.config('http_port'):
-        hookenv.open_port(hookenv.config('http_port'))
+    if config('http_port'):
+        open_port(config('http_port'))
     else:
-        hookenv.open_port(80)
+        open_port(80)
 
     status_set('active', 'GitLab is ready!')
 
@@ -55,45 +57,45 @@ def updateConfig(config):
 
     exturl = None
 
-    if hookenv.config('external_url'):
-        exturl = hookenv.config('external_url')
+    if config('external_url'):
+        exturl = config('external_url')
         if not exturl.startswith("http"):
             exturl = "http://" + exturl
 
-    if hookenv.config('external_url') and hookenv.config('http_port'):
+    if config('external_url') and config('http_port'):
         if exturl.endswith("/"):
             exturl = exturl[:-1]
 
-        exturl = exturl + ":" + hookenv.config('http_port')
+        exturl = exturl + ":" + config('http_port')
 
     modConfigNoEquals(filepath, 'external_url', exturl)
-    modConfig(filepath, 'gitlab_rails[\'gitlab_ssh_host\']', hookenv.config('ssh_host'))
-    modConfig(filepath, 'gitlab_rails[\'time_zone\']', hookenv.config('time_zone'))
-    modConfig(filepath, 'gitlab_rails[\'gitlab_email_from\']', hookenv.config('email_from'))
-    modConfig(filepath, 'gitlab_rails[\'gitlab_email_display_name\']', hookenv.config('from_email_name'))
-    modConfig(filepath, 'gitlab_rails[\'gitlab_email_reply_to\']', hookenv.config('reply_to_email'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_enable\']', hookenv.config('smtp_enable'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_address\']', hookenv.config('smtp_address'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_port\']', hookenv.config('smtp_port'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_user_name\']', hookenv.config('smtp_user_name'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_password\']', hookenv.config('smtp_password'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_domain\']', hookenv.config('smtp_domain'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_enable_starttls_auto\']', hookenv.config('smtp_enable_starttls_auto'))
-    modConfig(filepath, 'gitlab_rails[\'smtp_tls\']', hookenv.config('smtp_tls'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_enabled\']', hookenv.config('incoming_email_enabled'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_address\']', hookenv.config('incoming_email_address'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_email\']', hookenv.config('incoming_email_email'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_password\']', hookenv.config('incoming_email_password'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_host\']', hookenv.config('incoming_email_host'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_port\']', hookenv.config('incoming_email_port'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_ssl\']', hookenv.config('incoming_email_ssl'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_start_tls\']', hookenv.config('incoming_email_start_tls'))
-    modConfig(filepath, 'gitlab_rails[\'incoming_email_mailbox_name\']', hookenv.config('incoming_email_mailbox_name'))
-    modConfig(filepath, 'gitlab_rails[\'backup_path\']', hookenv.config('backup_path'))
-    modConfig(filepath, 'gitlab_rails[\'backup_keep_time\']', hookenv.config('backup_keep_time'))
+    modConfig(filepath, 'gitlab_rails[\'gitlab_ssh_host\']', config('ssh_host'))
+    modConfig(filepath, 'gitlab_rails[\'time_zone\']', config('time_zone'))
+    modConfig(filepath, 'gitlab_rails[\'gitlab_email_from\']', config('email_from'))
+    modConfig(filepath, 'gitlab_rails[\'gitlab_email_display_name\']', config('from_email_name'))
+    modConfig(filepath, 'gitlab_rails[\'gitlab_email_reply_to\']', config('reply_to_email'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_enable\']', config('smtp_enable'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_address\']', config('smtp_address'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_port\']', config('smtp_port'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_user_name\']', config('smtp_user_name'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_password\']', config('smtp_password'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_domain\']', config('smtp_domain'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_enable_starttls_auto\']', config('smtp_enable_starttls_auto'))
+    modConfig(filepath, 'gitlab_rails[\'smtp_tls\']', config('smtp_tls'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_enabled\']', config('incoming_email_enabled'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_address\']', config('incoming_email_address'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_email\']', config('incoming_email_email'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_password\']', config('incoming_email_password'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_host\']', config('incoming_email_host'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_port\']', config('incoming_email_port'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_ssl\']', config('incoming_email_ssl'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_start_tls\']', config('incoming_email_start_tls'))
+    modConfig(filepath, 'gitlab_rails[\'incoming_email_mailbox_name\']', config('incoming_email_mailbox_name'))
+    modConfig(filepath, 'gitlab_rails[\'backup_path\']', config('backup_path'))
+    modConfig(filepath, 'gitlab_rails[\'backup_keep_time\']', config('backup_keep_time'))
     modConfig(filepath, 'gitlab_rails[\'backup_upload_remote_directory\']',
-              hookenv.config('backup_upload_remote_directory'))
-    modConfig(filepath, 'gitlab_rails[\'backup_upload_connection\']', hookenv.config('backup_upload_connection'))
+              config('backup_upload_remote_directory'))
+    modConfig(filepath, 'gitlab_rails[\'backup_upload_connection\']', config('backup_upload_connection'))
 
     check_call(["gitlab-ctl", "reconfigure"])
 
@@ -142,7 +144,7 @@ def modConfig(File, Variable, Setting):
             _infile_var = str(line.split('=')[0].rstrip(' '))
             _infile_set = str(line.split('=')[1].lstrip(' ').rstrip())
             # only change the first matching occurrence #
-            if VarFound == False and _infile_var.rstrip(' ') == V:
+            if not VarFound and _infile_var.rstrip(' ') == V:
                 VarFound = True
                 # don't change it if it is already set #
                 if _infile_set.lstrip(' ') == S:
@@ -157,16 +159,16 @@ def modConfig(File, Variable, Setting):
         print("Variable '%s' not found.  Adding it to %s" % (V, File))
         with open(File, "a") as f:
             l = "%s = %s\n" % (V, S)
-            if (Setting is '' or Setting is None) and not l.lstrip(' ').startswith('#'):
+            if not Setting and not l.lstrip(' ').startswith('#'):
                 l = '#' + l
                 f.write(l)
-            elif (Setting is not '' or Setting is not None) and l.lstrip(' ').startswith('#'):
+            elif Setting and l.lstrip(' ').startswith('#'):
                 l = re.sub("#", "", l)
                 f.write(l)
-            elif (Setting is not '' or Setting is not None):
+            elif Setting is not '' or Setting is not None:
                 f.write(l)
 
-    elif AlreadySet == True:
+    elif AlreadySet:
         print("Variable '%s' unchanged" % (V))
     else:
         print("Variable '%s' modified to '%s'" % (V, S))
